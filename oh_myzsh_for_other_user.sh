a430f561840f5abd2b455d54ae92bb389e0e5421
jakiro2017 <totrantien@gmail.com>
2021-12-28 16:25:40 +0700

    get home

--- oh_myzsh_for_other_user.sh
+++ oh_myzsh_for_other_user.sh
@@ -1,5 +1,5 @@
 USER=root
-USER_HOME_PATH=$(zsh -c "cd ~$(printf %q $USER) && pwd")
+USER_HOME_PATH=$(getent passwd $USER_TARGET | cut -d: -f6)
 sudo ln -s $HOME/.zshrc   ${USER_HOME_PATH}/.zshrc
 sudo ln -s $HOME/.oh-my-zsh   ${USER_HOME_PATH}/.oh-my-zsh
 sudo ln -s $HOME/.p10k.zsh   ${USER_HOME_PATH}/.p10k.zsh
